e110029e481f5dff1f1f5d81efb9d0c523db32fb
len <dev@len-l.de>
2025-03-06 15:15:56 +0100

    remove obsolete unique_declarations

--- app/postprocess/__init__.py
+++ app/postprocess/__init__.py
@@ -1,19 +1,23 @@
+"""Post-process merged QASM-Program."""
+
 from openqasm3.ast import Program, QASMNode
 from openqasm3.printer import dumps
 
 from app.postprocess.sort_imports import SortImports
-from app.postprocess.unique_declarations import UniqueDeclarations
 
 
 def preprocess_str(program: Program) -> str:
+    """Return post-processed program as string."""
     return dumps(preprocess(program))
 
 
 def preprocess(program: Program) -> Program:
+    """Return post-processed program as AST."""
     tmp: QASMNode | None = None
-    for Transformer in (SortImports, UniqueDeclarations):
-        tmp = Transformer().visit(program)
+    for transformer in (SortImports,):
+        tmp = transformer().visit(program)
         if not isinstance(tmp, Program):
-            raise RuntimeError(f"{Transformer} returned {tmp}, not a Program")
+            msg = f"{transformer} returned {tmp}, not a Program"
+            raise TypeError(msg)
         program = tmp
     return program
