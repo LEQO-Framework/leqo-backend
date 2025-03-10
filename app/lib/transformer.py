59e53e13d37bebd09c35b82fc7e9de3845aeb19c
len <dev@len-l.de>
2025-03-10 17:35:21 +0100

    added generics to transformer

--- app/lib/transformer.py
+++ app/lib/transformer.py
@@ -1,23 +1,29 @@
-"""Fix QASMTransformer ignoring lists in lists."""
+"""Fix QASMTransformer ignoring lists in lists and tuples."""
 
-from typing import override
+from typing import TypeVar, override
 
 from openqasm3.ast import QASMNode
 from openqasm3.visitor import QASMTransformer
 
+T = TypeVar("T")
 
-class Transformer(QASMTransformer[None]):
-    """Fixes an issue in the parent, walk through lists in lists."""
+
+class Transformer(QASMTransformer[T]):
+    """Fixes an issue in the parent, walk through lists/tuples recursively."""
 
     @override
-    def generic_visit(self, node: QASMNode, context: None = None) -> QASMNode:
-        """Almost a clone of the parent method, but handles lists via list_visit."""
+    def generic_visit(self, node: QASMNode, context: T | None = None) -> QASMNode:
+        """Almost a clone of the parent method, but handles lists/tuples recursively."""
         for field, old_value in node.__dict__.items():
             if isinstance(old_value, list):
-                self.list_visit(old_value)
+                setattr(node, field, self.list_visit(old_value, context))
+            elif isinstance(old_value, tuple):
+                setattr(node, field, self.tuple_visit(old_value, context))
             elif isinstance(old_value, QASMNode):
                 new_node = (
-                    self.visit(old_value, context) if context else self.visit(old_value)
+                    self.visit(old_value, context)
+                    if context is not None
+                    else self.visit(old_value)
                 )
                 if new_node is None:
                     delattr(node, field)
@@ -25,19 +31,35 @@ class Transformer(QASMTransformer[None]):
                     setattr(node, field, new_node)
         return node
 
-    def list_visit(self, values: list[object]) -> None:
+    def list_visit(
+        self,
+        values: list[object],
+        context: T | None,
+    ) -> list[object]:
         """Recursively visits lists in lists."""
         new_values: list[object] = []
         for value in values:
             if isinstance(value, list):
-                self.list_visit(value)
-                new_values.append(value)
+                new_values.append(self.list_visit(value, context))
+            elif isinstance(value, tuple):
+                new_values.append(self.tuple_visit(value, context))
             elif isinstance(value, QASMNode):
-                new_value = self.visit(value)
+                new_value = (
+                    self.visit(value, context)
+                    if context is not None
+                    else self.visit(value)
+                )
                 if new_value is None:
                     continue
                 if not isinstance(new_value, QASMNode):
                     new_values.extend(new_value)
                     continue
                 new_values.append(new_value)
-        values[:] = new_values  # modify values inplace
+        return new_values
+
+    def tuple_visit(
+        self,
+        values: tuple[object, ...],
+        context: T | None,
+    ) -> tuple[object, ...]:
+        return tuple(self.list_visit(list(values), context))
--- tests/lib/test_transformer.py
+++ tests/lib/test_transformer.py
@@ -16,7 +16,7 @@ class AllToYDefault(QASMTransformer[None]):
         return node
 
 
-class AllToYFixed(Transformer):
+class AllToYFixed(Transformer[None]):
     """Like AllToYDefault but inherit from Transformer."""
 
     def visit_Identifier(self, node: Identifier) -> Identifier:
@@ -59,3 +59,53 @@ def test_switch() -> None:
     fixed = normalize(dumps(AllToYFixed().visit(parse(before))))
     assert true != previous
     assert true == fixed
+
+
+class AllToYFixedContext(Transformer[bool]):
+    """Check if context works."""
+
+    def visit_Identifier(
+        self,
+        node: Identifier,
+        context: bool | None = None,
+    ) -> Identifier:
+        """Replace name of all Identifiers with y."""
+        if context is True:
+            node.name = "y"
+        return node
+
+
+def test_context() -> None:
+    """Check if Transformer can handle variables in indices."""
+    before = normalize("""
+    x q;
+    """)
+    true = normalize("""
+    y y;
+    """)
+    none = normalize(dumps(AllToYFixedContext().visit(parse(before), False)))
+    replaced = normalize(dumps(AllToYFixedContext().visit(parse(before), True)))
+    assert before == none
+    assert true == replaced
+
+
+def test_all() -> None:
+    """Check if Transformer can handle variables in indices."""
+    before = normalize("""
+    switch (i) {
+        case 1, B, C {
+            x q[I];
+            }
+        }
+    """)
+    true = normalize("""
+    switch (y) {
+        case 1, y, y {
+            y y[y];
+            }
+        }
+    """)
+    none = normalize(dumps(AllToYFixedContext().visit(parse(before), False)))
+    replaced = normalize(dumps(AllToYFixedContext().visit(parse(before), True)))
+    assert before == none
+    assert true == replaced
