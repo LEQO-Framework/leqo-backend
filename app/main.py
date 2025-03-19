5cbc88cb20940c0fc6e30dd9f4071849172b931e
Arne <112285398+arne-gbr@users.noreply.github.com>
2025-03-19 14:36:11 +0100

    Add CORS support
    Closes #43

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.model.CompileRequest import CompileRequest
 
 app = FastAPI()
+
+origins: list[str] = [ # ToDo: wich origins do we need?
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:4242",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 results: dict[str, str] = {}
 
