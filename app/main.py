d5f19a51394563e962e7cf06afb2393a7516348d
Arne <112285398+arne-gbr@users.noreply.github.com>
2025-03-19 14:35:17 +0100

    Add Endpoint to get result

--- app/main.py
+++ app/main.py
@@ -4,7 +4,15 @@ from app.model.CompileRequest import CompileRequest
 
 app = FastAPI()
 
+results: dict[str, str] = {}
+
 
 @app.post("/compile")
-def compile(body: CompileRequest) -> CompileRequest:
-    return body
+def compile(body: CompileRequest) -> str:
+    id = body.metadata.id
+    results[id] = body.model_dump_json()
+    return f"GET /result/{id}"
+
+@app.get("/result/{id}")
+def result(id: str) -> str:
+    return results[id]
