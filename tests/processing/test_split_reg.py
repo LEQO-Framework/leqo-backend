5810f3286121c166d6034bea502dbda64c128f48
len <dev@len-l.de>
2025-03-19 18:28:58 +0100

    started with gate_split

--- tests/processing/test_split_reg.py
+++ tests/processing/test_split_reg.py
@@ -32,3 +32,54 @@ def test_split_qubit_declaration() -> None:
     real = apply_transformer(before, SplitRegAndIOParse())
     print(normalize(real))
     assert normalize(expected) == normalize(real)
+
+
+def test_parse_qubit_reg_annotation() -> None:
+    pass
+
+
+def test_split_unindexed_qubit_reg_gates() -> None:
+    before = """
+    qubit[3] q0;
+    qubit q1;
+    g0 q0;
+    g1 q0, q1;
+    """
+    expected = """
+    qubit q0_part0;
+    qubit q0_part1;
+    qubit q0_part2;
+    qubit q1;
+    g0 q0_part0;
+    g0 q0_part1;
+    g0 q0_part2;
+    g1 q0_part0 q1;
+    g1 q0_part1 q1;
+    g1 q0_part2 q1;
+    """
+    real = apply_transformer(before, SplitRegAndIOParse())
+    print(normalize(real))
+    assert normalize(expected) == normalize(real)
+
+def test_split_unindexed_qubit_reg_gates() -> None:
+    before = """
+    qubit[3] q0;
+    qubit[3] q1;
+    g0 q0[0:2:2];
+    g1 q0[1:2], q1[0:1];
+    """
+    ast = parse(before)
+    # breakpoint()
+    expected = """
+    qubit q0_part0;
+    qubit q0_part1;
+    qubit q0_part2;
+    qubit q1_part0;
+    qubit q1_part1;
+    qubit q1_part2;
+    g0 q0_part0;
+    g0 q0_part2;
+    """
+    real = apply_transformer(before, SplitRegAndIOParse())
+    print(normalize(real))
+    assert normalize(expected) == normalize(real)
