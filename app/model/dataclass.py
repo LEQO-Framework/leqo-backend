645bf40f808eb8baaad2b639524a44bea7826e64
len <dev@len-l.de>
2025-03-29 14:12:08 +0100

    dataclass: constructors

--- app/model/dataclass.py
+++ app/model/dataclass.py
@@ -5,10 +5,18 @@ class SingleQubitInputInfo:
     id: int
     position: int
 
+    def __init__(self, input_id: int, position: int) -> None:
+        self.id = input_id
+        self.position = position
+
 
 class SingleQubitOutputInfo:
     id: int
     position: int
+
+    def __init__(self, output_id: int, position: int) -> None:
+        self.id = output_id
+        self.position = position
 
 
 class SingleQubitIOInfo:
@@ -18,12 +26,16 @@ class SingleQubitIOInfo:
 
 
 class IOInfo:
-    declaration_to_id: dict["str", list[int]]
+    declaration_to_id: dict[str, list[int]]
     id_to_info: dict[int, SingleQubitIOInfo]
 
-    def __init__(self) -> None:
-        self.declaration_to_id = {}
-        self.id_to_info = {}
+    def __init__(
+        self,
+        declaration_to_id: dict[str, list[int]] | None = None,
+        id_to_info: dict[int, SingleQubitIOInfo] | None = None,
+    ) -> None:
+        self.declaration_to_id = declaration_to_id or {}
+        self.id_to_info = id_to_info or {}
 
     @override
     def __eq__(self, value: object, /) -> bool:
