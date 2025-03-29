b2eec9d79022d7663565be39d2cf555be8ad7eff
len <dev@len-l.de>
2025-03-29 11:50:18 +0100

    use simpler io-annotation system

--- app/lib/ast_utils.py
+++ app/lib/ast_utils.py
@@ -1,3 +1,4 @@
+from io import UnsupportedOperation
 from typing import TypeVar
 
 from openqasm3.ast import (
@@ -47,37 +48,19 @@ def annotate(node: TQasmStatement, annotations: list[Annotation]) -> TQasmStatem
     return node
 
 
-def parse_io_annotation(annotation: Annotation) -> list[int]:
-    """Parses the :attr:`~openqasm3.ast.Annotation.command` of a `@leqo.input` or `@leqo.output` :class:`~openqasm3.ast.Annotation`.
+def parse_io_annotation(annotation: Annotation) -> int:
+    """Parse the :attr:`~openqasm3.ast.Annotation.command` of a `@leqo.input` or `@leqo.output` :class:`~openqasm3.ast.Annotation`.
 
     :param annotation: The annotation to parse
-    :return: The parsed indices
+    :return: The indices
     """
     command = annotation.command and annotation.command.strip()
 
     if not command:
-        return []
+        msg = f"Annotation of type {type(annotation)} without index was found."
+        raise UnsupportedOperation(msg)
 
-    result: list[int] = []
-    for segment in command.split(","):
-        range_elements = [int(x.strip()) for x in segment.split("-")]
-        match len(range_elements):
-            case 0:
-                raise ValueError("Detected empty segment")
-
-            case 1:
-                result.append(range_elements[0])
-
-            case 2:
-                if range_elements[0] > range_elements[1]:
-                    raise ValueError("Start of range must be <= end of range")
-
-                result.extend(range(range_elements[0], range_elements[1] + 1))
-
-            case _:
-                raise ValueError("A range may only contain 2 integers")
-
-    return result
+    return int(command)
 
 
 def parse_range_definition(range_def: RangeDefinition, length: int) -> list[int]:
