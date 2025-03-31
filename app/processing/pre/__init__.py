61219869f7389f55c991360c14d42b8538802dc2
len <dev@len-l.de>
2025-03-31 18:17:36 +0200

    use io_parse in pre/__init__
    Co-authored-by: Johannes <jojoinfo@t-online.de>

--- app/processing/pre/__init__.py
+++ app/processing/pre/__init__.py
@@ -1,17 +1,25 @@
-from openqasm3.ast import Program
+from openqasm3.ast import Program, QASMNode
 
 from app.processing.graph import SectionInfo
+from app.processing.pre.io_parser import IOParse
 from app.processing.pre.renaming import RenameRegisterTransformer
 
 
 def preprocess(program: Program, section_info: SectionInfo) -> Program:
-    """
-    Runs an openqasm3 snippet through the preprocessing pipeline.
+    """Run an openqasm3 snippet through the preprocessing pipeline.
 
     :param program: A valid openqasm3 program (as AST) to preprocess.
     :param section_info: MetaData of the section to preprocess.
     :return: The preprocessed program.
     """
 
-    program = RenameRegisterTransformer().visit(program, section_info)
-    return program  # noqa: RET504 # Ignore because QASMTransformer.visit returns Any
+    def to_program(node: QASMNode | None) -> Program:
+        """Cast to Program or raise error."""
+        if not isinstance(node, Program):
+            msg = f"Tried to cast {type(node)} to Program."
+            raise TypeError(msg)
+        return node
+
+    program = to_program(RenameRegisterTransformer().visit(program, section_info))
+    program = to_program(IOParse(section_info.io).visit(program))
+    return program
