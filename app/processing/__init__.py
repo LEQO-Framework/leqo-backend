83255496d24df7021b6186f964d2d84bc4e59bd1
len <dev@len-l.de>
2025-03-31 21:11:17 +0200

    fix tests + dummy implementation
    Co-authored-by: Johannes <jojoinfo@t-online.de>

--- app/processing/__init__.py
+++ app/processing/__init__.py
@@ -1,27 +1,33 @@
+from graphlib import TopologicalSorter
 from io import StringIO
 
 from openqasm3.ast import Pragma, Program, Statement
 
 from app.openqasm3.ast import CommentStatement
 from app.openqasm3.printer import LeqoPrinter
-from app.processing.graph import ProgramNode, SectionInfo
+from app.processing.graph import IOConnection, ProgramNode, SectionInfo
 from app.processing.post import postprocess
 from app.processing.pre import preprocess
 
 
-def merge_nodes(nodes: set[ProgramNode], connections: set[IOConnection]) -> Program:
+def merge_nodes(nodes: set[ProgramNode], edges: set[IOConnection]) -> Program:
     """Create a unified :class:`openqasm3.ast.Program` from a modelled graph with attached qasm implementation snippets.
 
     :param nodes: set containing the program snippets
     :param connections: set containing the input/output connections
     :return: The unifed qasm program
     """
-
     all_statements: list[Statement | Pragma] = []
+    topologie: TopologicalSorter[ProgramNode] = TopologicalSorter()
+    for node in nodes:
+        topologie.add(node)
+
+    for edge in edges:
+        topologie.add(edge.target[0], edge.source[0])
 
     # TODO: This can be parallelized
     # TODO: this does not sort the nodes the right way, this will need io-info
-    for i, node in enumerate(nodes):
+    for i, node in enumerate(topologie.static_order()):
         all_statements.append(CommentStatement(f"Start node {node.id}"))
 
         ast = preprocess(node.implementation.ast, SectionInfo(i, node))
@@ -34,13 +40,11 @@ def merge_nodes(nodes: set[ProgramNode], connections: set[IOConnection]) -> Prog
 
 
 def print_program(program: Program) -> str:
-    """
-    Prints the given program as a string.
+    """Prints the given program as a string.
 
     :param program: The program to print
     :return: The program as a string
     """
-
     result = StringIO()
     LeqoPrinter(result).visit(program)
     return result.getvalue()
--- tests/processing/test_init.py
+++ tests/processing/test_init.py
@@ -1,8 +1,7 @@
-from graphlib import TopologicalSorter
 from textwrap import dedent
 
 from app.processing import merge_nodes, print_program
-from app.processing.graph import ProgramNode, QasmImplementation
+from app.processing.graph import IOConnection, ProgramNode, QasmImplementation
 
 
 def test_merge_nodes() -> None:
@@ -10,10 +9,14 @@ def test_merge_nodes() -> None:
     node1 = ProgramNode("42", QasmImplementation.create("qubit a;"))
     node2 = ProgramNode("420", QasmImplementation.create("qubit b;"))
 
-    graph = TopologicalSorter[ProgramNode]()
-    graph.add(node0, node1, node2)
+    nodes = {node0, node1, node2}
+    edges = {
+        IOConnection((node1, 0), (node0, 0), 0),
+        IOConnection((node2, 0), (node0, 0), 0),
+        IOConnection((node1, 0), (node2, 0), 0),
+    }
 
-    program = merge_nodes(graph)
+    program = merge_nodes(nodes, edges)
     result = print_program(program)
     assert result == dedent("""\
         OPENQASM 3.1;
