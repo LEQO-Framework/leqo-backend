bda220935611e71992e89f63443500d6d71bbcd5
len <dev@len-l.de>
2025-04-02 18:35:12 +0200

    more tests

--- tests/processing/test_utils.py
+++ tests/processing/test_utils.py
@@ -57,19 +57,25 @@ def test_parse_qasm_index() -> None:
         assert result == expected
 
     assert_parse("[0]", 1, [0])
+    assert_parse("[3]", 5, [3])
     assert_parse("[0:3]", 4, [0, 1, 2, 3])
+    assert_parse("[0:2]", 4, [0, 1, 2])
+    assert_parse("[1:2]", 4, [1, 2])
     assert_parse("[0:-1]", 3, [0, 1, 2])
+    assert_parse("[1:-1]", 3, [1, 2])
     assert_parse("[0:2:5]", 10, [0, 2, 4])
+    assert_parse("[0:3:9]", 10, [0, 3, 6, 9])
     assert_parse("[3:-1:1]", 10, [3, 2, 1])
-    assert_parse("[{1, 2, 4}]", 10000, [1, 2, 4])
-    assert_parse("[{0, 1, 2, 3}][0:2][0]", 4, [0])
-    assert_parse("[{3, 2, 1, 0}][1:3][0:1]", 4, [2, 1])
+    assert_parse("[8:-2:0]", 10, [8, 6, 4, 2, 0])
     assert_parse("[1:]", 3, [1, 2])
     assert_parse("[:1]", 3, [0, 1])
     assert_parse("[:]", 3, [0, 1, 2])
+    assert_parse("[{1, 2, 4}]", 10000, [1, 2, 4])
+    assert_parse("[{0, 1, 2, 3}][0:2][0]", 4, [0])
+    assert_parse("[{3, 2, 1, 0}][1:3][0:1]", 4, [2, 1])
     with pytest.raises(IndexError):
         assert_parse("[1000]", 4, [])
     with pytest.raises(IndexError):
-        assert_parse("[0:1000]", 4, [0, 1, 2, 3])
+        assert_parse("[0:1000]", 4, [])
     with pytest.raises(IndexError):
-        assert_parse("[{3, 2}][3]", 4, [0, 1, 2, 3])
+        assert_parse("[{3, 2}][3]", 4, [])
