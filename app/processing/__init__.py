22ce2081032411267ac48f7c12a93a990aa0c9b2
len-01 <165215453+len-01@users.noreply.github.com>
2025-04-02 18:01:43 +0200

    refactor: merging interface (#60)
    Co-authored-by: Johannes <jojoinfo@t-online.de>
    Co-authored-by: Lukas Kurz <lukas@shortdev.de>

--- app/processing/__init__.py
+++ app/processing/__init__.py
@@ -1,46 +1,67 @@
-from graphlib import TopologicalSorter
 from io import StringIO
+from uuid import uuid4
 
+from networkx import topological_sort
 from openqasm3.ast import Pragma, Program, Statement
 
 from app.openqasm3.ast import CommentStatement
+from app.openqasm3.parser import leqo_parse
 from app.openqasm3.printer import LeqoPrinter
-from app.processing.graph import ProgramNode, SectionInfo
+from app.processing.graph import (
+    ProcessedProgramNode,
+    ProgramGraph,
+    ProgramNode,
+    SectionInfo,
+)
 from app.processing.post import postprocess
-from app.processing.pre import preprocess
+from app.processing.pre import preprocess as preprocess_impl
+from app.utils import opt_call
 
 
-def merge_nodes(graph: TopologicalSorter[ProgramNode]) -> Program:
+def preprocess(node: ProgramNode) -> ProcessedProgramNode:
     """
-    Creates a unified :class:`openqasm3.ast.Program` from a modelled graph with attached qasm implementation snippets.
+    Preprocess the given node.
 
-    :param graph: Visual model of the qasm program
-    :return: The unifed qasm program
+    :param node: Node to preprocess
+    :return: Preprocessed node
     """
 
+    section_info = SectionInfo(uuid4())
+    implementation = preprocess_impl(leqo_parse(node.implementation), section_info)
+
+    uncompute_implementation = opt_call(leqo_parse, node.uncompute_implementation)
+
+    return ProcessedProgramNode(
+        node, implementation, section_info, uncompute_implementation
+    )
+
+
+def merge_nodes(graph: ProgramGraph) -> Program:
+    """Create a unified :class:`openqasm3.ast.Program` from a modelled graph with attached qasm implementation snippets.
+
+    :param graph: Graph of all nodes representing the program
+    :return: The unified qasm program
+    """
     all_statements: list[Statement | Pragma] = []
 
-    # ToDo: This can be parallelized
-    for i, node in enumerate(graph.static_order()):
-        all_statements.append(CommentStatement(f"Start node {node.id}"))
+    # TODO: this does not sort the nodes the right way, this will need io-info
+    for node in topological_sort(graph):
+        all_statements.append(CommentStatement(f"Start node {node.name}"))
 
-        ast = preprocess(node.implementation.ast, SectionInfo(i, node))
-        all_statements.extend(ast.statements)
+        all_statements.extend(graph.get_data_node(node).implementation.statements)
 
-        all_statements.append(CommentStatement(f"End node {node.id}"))
+        all_statements.append(CommentStatement(f"End node {node.name}"))
 
     merged_program = Program(all_statements, version="3.1")
     return postprocess(merged_program)
 
 
 def print_program(program: Program) -> str:
-    """
-    Prints the given program as a string.
+    """Prints the given program as a string.
 
     :param program: The program to print
     :return: The program as a string
     """
-
     result = StringIO()
     LeqoPrinter(result).visit(program)
     return result.getvalue()
