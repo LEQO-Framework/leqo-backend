d0914d74867b444f1d13d2ae98e8857b152f3295
len <dev@len-l.de>
2025-04-05 22:04:34 +0200

    small test refactor

--- tests/processing/pre/test_io_parse.py
+++ tests/processing/pre/test_io_parse.py
@@ -263,6 +263,64 @@ def test_input_index_weird_order() -> None:
     assert expected == actual
 
 
+def test_all() -> None:
+    code = normalize_qasm_string("""
+    @leqo.input 0
+    qubit[5] q0;
+    @leqo.input 1
+    qubit[5] q1;
+    @leqo.dirty
+    qubit q2;
+
+    let a = q1[{4, 3, 2, 1, 0}];
+
+    @leqo.output 0
+    let _out0 = q0[0] ++ q1[0];
+    @leqo.output 1
+    let _out1 = q0[1] ++ a[1]; // a[1] == q1[3]
+
+    @leqo.reusable
+    let _reuse = q0[2:4];
+    """)
+    expected = IOInfo(
+        declaration_to_ids={
+            "q0": [0, 1, 2, 3, 4],
+            "q1": [5, 6, 7, 8, 9],
+            "q2": [10],
+        },
+        id_to_info={
+            0: QubitAnnotationInfo(
+                input=QubitInputInfo(0, 0),
+                output=QubitOutputInfo(0, 0),
+            ),
+            1: QubitAnnotationInfo(
+                input=QubitInputInfo(0, 1),
+                output=QubitOutputInfo(1, 0),
+            ),
+            2: QubitAnnotationInfo(input=QubitInputInfo(0, 2), reusable=True),
+            3: QubitAnnotationInfo(input=QubitInputInfo(0, 3), reusable=True),
+            4: QubitAnnotationInfo(input=QubitInputInfo(0, 4), reusable=True),
+            5: QubitAnnotationInfo(
+                input=QubitInputInfo(1, 0),
+                output=QubitOutputInfo(0, 1),
+            ),
+            6: QubitAnnotationInfo(input=QubitInputInfo(1, 1)),
+            7: QubitAnnotationInfo(input=QubitInputInfo(1, 2)),
+            8: QubitAnnotationInfo(
+                input=QubitInputInfo(1, 3),
+                output=QubitOutputInfo(1, 1),
+            ),
+            9: QubitAnnotationInfo(input=QubitInputInfo(1, 4)),
+            10: QubitAnnotationInfo(dirty=True),
+        },
+        input_to_ids={0: [0, 1, 2, 3, 4], 1: [5, 6, 7, 8, 9]},
+        output_to_ids={0: [0, 5], 1: [1, 8]},
+    )
+    actual = IOInfo()
+    ParseAnnotationsVisitor(actual).visit(parse(code))
+    assert expected == actual
+
+
 def test_raise_on_missing_io_index() -> None:
     code = """
     @leqo.input 0
@@ -449,61 +507,3 @@ def test_raise_on_double_output_declaration_on_single_qubit() -> None:
         match="alias b tries to overwrite already declared output",
     ):
         ParseAnnotationsVisitor(IOInfo()).visit(parse(code))
-
-
-def test_all() -> None:
-    code = normalize_qasm_string("""
-    @leqo.input 0
-    qubit[5] q0;
-    @leqo.input 1
-    qubit[5] q1;
-    @leqo.dirty
-    qubit q2;
-
-    let a = q1[{4, 3, 2, 1, 0}];
-
-    @leqo.output 0
-    let _out0 = q0[0] ++ q1[0];
-    @leqo.output 1
-    let _out1 = q0[1] ++ a[1]; // a[1] == q1[3]
-
-    @leqo.reusable
-    let _reuse = q0[2:4];
-    """)
-    expected = IOInfo(
-        declaration_to_ids={
-            "q0": [0, 1, 2, 3, 4],
-            "q1": [5, 6, 7, 8, 9],
-            "q2": [10],
-        },
-        id_to_info={
-            0: QubitAnnotationInfo(
-                input=QubitInputInfo(0, 0),
-                output=QubitOutputInfo(0, 0),
-            ),
-            1: QubitAnnotationInfo(
-                input=QubitInputInfo(0, 1),
-                output=QubitOutputInfo(1, 0),
-            ),
-            2: QubitAnnotationInfo(input=QubitInputInfo(0, 2), reusable=True),
-            3: QubitAnnotationInfo(input=QubitInputInfo(0, 3), reusable=True),
-            4: QubitAnnotationInfo(input=QubitInputInfo(0, 4), reusable=True),
-            5: QubitAnnotationInfo(
-                input=QubitInputInfo(1, 0),
-                output=QubitOutputInfo(0, 1),
-            ),
-            6: QubitAnnotationInfo(input=QubitInputInfo(1, 1)),
-            7: QubitAnnotationInfo(input=QubitInputInfo(1, 2)),
-            8: QubitAnnotationInfo(
-                input=QubitInputInfo(1, 3),
-                output=QubitOutputInfo(1, 1),
-            ),
-            9: QubitAnnotationInfo(input=QubitInputInfo(1, 4)),
-            10: QubitAnnotationInfo(dirty=True),
-        },
-        input_to_ids={0: [0, 1, 2, 3, 4], 1: [5, 6, 7, 8, 9]},
-        output_to_ids={0: [0, 5], 1: [1, 8]},
-    )
-    actual = IOInfo()
-    ParseAnnotationsVisitor(actual).visit(parse(code))
-    assert expected == actual
