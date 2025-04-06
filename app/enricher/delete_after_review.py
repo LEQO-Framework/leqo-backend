cd754ea132949fb877a16a0c3eedc9b95ca6e451
Louis Radek <radek.louis@gmail.com>
2025-04-06 18:05:15 +0200

    feat: add enricher setup

---
+++ app/enricher/delete_after_review.py
@@ -0,0 +1,40 @@
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# This file is only meant for testing purposes
+# Delete this file after the review
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+from sqlalchemy import Column, Integer, Text
+from app.enricher.models import Base
+from sqlalchemy import select
+from sqlalchemy.orm import sessionmaker
+
+from app.enricher.engine import engine
+from app.enricher.utils import reset_database
+
+class QuasmImplementation(Base):
+    __tablename__ = 'qasm_impl'
+
+    id = Column(Integer(), primary_key=True)
+    quasm = Column(Text, nullable=False)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def createQuasmImplementation(quasm: str) -> None:
+    with Session() as session:
+        qasmImplementation = QuasmImplementation(quasm=quasm)
+        session.add(qasmImplementation)
+        session.commit()
+
+def findQuasmImplementation(searchTerm: str):
+    with Session() as session:
+        query = select(QuasmImplementation).where(QuasmImplementation.quasm == searchTerm)
+        result = session.execute(query)
+        return [(row[0].id, row[0].quasm) for row in result.all()]
+
+def demo():
+    reset_database(engine)
+    createQuasmImplementation("x q[0]")
+    createQuasmImplementation("cx q[1], q[2]")
+    createQuasmImplementation("c[0] = measure q[2]")
+    return findQuasmImplementation("x q[0]")
---
+++ app/enricher/engine.py
@@ -0,0 +1,25 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+
+from app.enricher.utils import create_database
+
+load_dotenv()
+
+try:
+    url = URL.create(
+        drivername=os.environ['SQLALCHEMY_DRIVER'],
+        username=os.environ['POSTGRES_USER'],
+        password=os.environ['POSTGRES_PASSWORD'],
+        host=os.environ['POSTGRES_HOST'],
+        port=os.environ['POSTGRES_PORT'],
+        database=os.environ['POSTGRES_DB']
+    )
+    engine = create_engine(url)
+    create_database(engine)
+except KeyError as e:
+    raise RuntimeError(f"Missing required environment variable: {e}") from e
+except Exception as e:
+    raise RuntimeError(f"Failed to create the database engine: {e}") from e
