f49b15c06b406d4b022e7ff505f61dd72208e252
Louis Radek <radek.louis@gmail.com>
2025-04-06 18:18:37 +0200

    feat: add formatting

--- app/enricher/utils.py
+++ app/enricher/utils.py
@@ -6,8 +6,10 @@ from app.enricher.models import Base
 def create_database(engine: Engine) -> None:
     Base.metadata.create_all(engine)
 
+
 def drop_database(engine: Engine) -> None:
     Base.metadata.drop_all(engine)
+
 
 def reset_database(engine: Engine) -> None:
     drop_database(engine)
