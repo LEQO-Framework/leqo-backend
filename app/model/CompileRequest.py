8dadf42e739c37dba685f8c614874a1748cff88c
Arne <112285398+arne-gbr@users.noreply.github.com>
2025-04-09 21:12:07 +0200

    Update CompileRequest.py
    Co-Authored-By: Lukas Kurz <lukas@shortdev.de>

--- app/model/CompileRequest.py
+++ app/model/CompileRequest.py
@@ -3,9 +3,9 @@ This module defines the data models for compile requests.
 It provides classes to model metadata, node data, and the complete compile request.
 """
 
-from enum import StrEnum
+from typing import Annotated, Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class MetaData(BaseModel):
@@ -17,57 +17,146 @@ class MetaData(BaseModel):
     name: str
     description: str
     author: str
-    timestamp: str
+    optimizeWidth: Annotated[int, Field(ge=0)] | None = None
+    optimizeDepth: Annotated[int, Field(ge=0)] | None = None
 
 
-class NodeIdRef(
-    BaseModel
-):  # ToDo: remove after frontend changed inputs to type list[str]
-    """
-    Models a reference object containing the node ID.
-    """
-
-    id: str
-
-
-class NodeType(StrEnum):
-    """
-    Enumeration of the various node types.
-    """
-
-    POSITION_NODE = "positionNode"
-    STATE_PREPARATION_NODE = "statePreparationNode"
-    GATE_NODE = "gateNode"
-    OPERATION_NODE = "operationNode"
-    ANCILLA_NODE = "ancillaNode"
-    CLASSICAL_OUTPUT_OPERATION_NODE = "classicalOutputOperationNode"
-    ARITHMETIC_OPERATOR_NODE = "arithmeticOperatorNode"
-    MEASUREMENT_NODE = "measurementNode"
-
-
-class NodeData(BaseModel):
-    """
-    Contains specific data for a node.
-    """
-
-    label: str
-    inputs: list[NodeIdRef]  # ToDo: change to list[str]
-    implementation: str
-    implementationType: str
-    uncomputeImplementationType: str
-    uncomputeImplementation: str
-    indices: str | None = None
-    outputIdentifier: str
-
-
-class Node(BaseModel):
+class _BaseNode(BaseModel):
     """
     Models a node within a compile request.
     """
 
     id: str
-    type: NodeType
-    data: NodeData
+    label: str | None = None
+
+
+class ImplementationNode(_BaseNode):
+    type: Literal["implementation"] = "implementation"
+    implementation: str
+
+
+# region Boundary Nodes
+class EncodeValueNode(_BaseNode):
+    type: Literal["encode"] = "encode"
+    encoding: Literal["amplitude", "angle", "basis", "custom", "matrix", "schmidt"]
+    bounds: int = Field(ge=1)  # ToDo: ???
+
+
+class PrepareStateNode(_BaseNode):
+    type: Literal["prepare"] = "prepare"
+    size: int = Field(ge=1)
+    quantumState: Literal["ϕ+", "ϕ-", "ψ+", "ψ-", "custom", "ghz", "uniform", "w"]
+
+
+class MeasurementNode(_BaseNode):
+    type: Literal["measure"] = "measure"
+    indices: list[Annotated[int, Field(ge=0)]]
+
+
+BoundaryNode = EncodeValueNode | PrepareStateNode | MeasurementNode
+# endregion
+
+
+# region Circuit Nodes
+class QubitNode(_BaseNode):
+    type: Literal["qubit"] = "qubit"
+    size: int = Field(default=1, ge=1)
+
+
+class GateNode(_BaseNode):
+    type: Literal["gate"] = "gate"
+    gate: Literal["cnot", "toffoli", "h", "rx", "ry", "rz", "x", "y", "z"]
+
+
+CircuitNode = QubitNode | GateNode
+# endregion
+
+
+# region Literals
+# ToDo: Array support?
+
+
+class BitLiteralNode(_BaseNode):
+    type: Literal["bit"] = "bit"
+    value: Literal[0, 1]
+
+
+class BoolLiteralNode(_BaseNode):
+    type: Literal["bool"] = "bool"
+    value: bool
+
+
+class IntLiteralNode(_BaseNode):
+    type: Literal["int"] = "int"
+    bitSize: int = Field(default=32, ge=1)
+    value: int
+
+
+class FloatLiteralNode(_BaseNode):
+    type: Literal["float"] = "float"
+    bitSize: int = Field(default=32, ge=1)
+    value: float
+
+
+LiteralNode = BitLiteralNode | BoolLiteralNode | IntLiteralNode | FloatLiteralNode
+# endregion
+
+
+# region ControlFlow
+class IfThenElseNode(_BaseNode):
+    type: Literal["if-then-else"] = "if-then-else"
+    # ToDo: Condition?
+
+
+class RepeatNode(_BaseNode):
+    type: Literal["repeat"] = "repeat"
+    # ToDo: ???
+
+
+ControlFlowNode = IfThenElseNode | RepeatNode
+# endregion
+
+
+# region Operator
+class OperatorNode(_BaseNode):
+    type: Literal["operator"] = "operator"
+    operator: Literal[
+        "+",
+        "-",
+        "*",
+        "/",
+        "**",
+        "|",
+        "&",
+        "~",
+        "^",
+        "<",
+        "<=",
+        ">",
+        ">=",
+        "==",
+        "!=",
+        "min",
+        "max",
+        "search",  # ToDo: ?
+    ]
+
+
+# endregion
+
+Node = (
+    ImplementationNode
+    | BoundaryNode
+    | CircuitNode
+    | LiteralNode
+    | ControlFlowNode
+    | OperatorNode
+)
+
+
+class Edge(BaseModel):
+    source: tuple[str, Annotated[int, Field(ge=0)]]
+    target: tuple[str, Annotated[int, Field(ge=0)]]
 
 
 class CompileRequest(BaseModel):
@@ -76,4 +165,5 @@ class CompileRequest(BaseModel):
     """
 
     metadata: MetaData
-    nodes: list[Node]
+    nodes: list[Annotated[Node, Field(discriminator="type")]]
+    edges: list[Edge]
