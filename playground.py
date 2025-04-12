037d1ec1de4de0d3bbdc5f543175a02224fe28c2
len <dev@len-l.de>
2025-04-12 17:35:03 +0200

    added uncompute to algo

--- playground.py
+++ playground.py
@@ -78,37 +78,44 @@ def random_graph(size: int) -> ProgramGraph:
 class AlgoPerf(ABC):
     graph: ProgramGraph
     added_edges: list[AncillaConnection]
+    uncomputed: dict[ProgramNode, bool]
     performance: int
 
     def __init__(self, graph: ProgramGraph) -> None:
         self.graph = graph
         self.added_edges = []
+        self.uncomputed = {n: False for n in self.graph.nodes()}
         self.performance = 0
 
     def add_edge(self, edge: AncillaConnection) -> None:
         self.graph.append_edge(edge)
         self.performance += len(edge.source)
 
+    def uncompute_node(self, node: ProcessedProgramNode) -> None:
+        self.uncomputed[node.raw] = True
+
     @abstractmethod
-    def compute(self) -> int:
-        return self.performance
+    def compute(self) -> tuple[int, int]:
+        return self.performance, len([k for k, v in self.uncomputed.items() if v])
 
 
 class DummyAlgo(AlgoPerf):
     @override
-    def compute(self) -> int:
+    def compute(self) -> tuple[int, int]:
         return super().compute()
 
 
 class NoPredDummy(AlgoPerf):
     reusable: list[ProcessedProgramNode]
     dirty: list[ProcessedProgramNode]
+    uncomputable: list[ProcessedProgramNode]
     nopred: list[ProcessedProgramNode]
 
     def __init__(self, graph: ProgramGraph) -> None:
         super().__init__(graph)
         self.reusable = []
         self.dirty = []
+        self.uncomputable = []
         self.nopred = [
             self.graph.get_data_node(n)
             for n, pred in self.graph.pred.items()
@@ -128,7 +135,7 @@ class NoPredDummy(AlgoPerf):
         return self.nopred.pop()
 
     @override
-    def compute(self) -> int:
+    def compute(self) -> tuple[int, int]:
         while len(self.nopred) > 0:
             node = self.get_and_remove_next_nopred()
             self.nopred.extend(self.remove_node(node))
@@ -150,11 +157,59 @@ class NoPredDummy(AlgoPerf):
                 )
                 if len(possible_source_ids) == 0:
                     self.dirty.remove(source)
+            while len(need_dirty) > 0 and len(self.uncomputable) > 0:
+                source = self.uncomputable[0]
+                possible_source_ids = (
+                    source.info.io.qubits.returned_reusable_after_uncompute_ids
+                )
+                size = min(len(need_dirty), len(possible_source_ids))
+                target_ids = need_dirty[:size]
+                need_dirty = need_dirty[size:]
+                source_ids = possible_source_ids[:size]
+                possible_source_ids = possible_source_ids[size:]
+                self.add_edge(
+                    AncillaConnection(
+                        (source.raw, source_ids),
+                        (node.raw, target_ids),
+                    ),
+                )
+                if len(possible_source_ids) == 0:
+                    self.uncomputable.remove(source)
 
             need_reusable = node.info.io.qubits.required_reusable_ids
-            while len(need_reusable) > 0 and len(self.reusable) > 0:
-                source = self.reusable[0]
-                possible_source_ids = source.info.io.qubits.returned_reusable_ids
+            while len(need_reusable) > 0 and (
+                len(self.uncomputable) > 0 or len(self.reusable) > 0
+            ):
+                while len(need_reusable) > 0 and len(self.reusable) > 0:
+                    source = self.reusable[0]
+                    possible_source_ids = source.info.io.qubits.returned_reusable_ids
+                    size = min(len(need_reusable), len(possible_source_ids))
+                    target_ids = need_reusable[:size]
+                    need_reusable = need_reusable[size:]
+                    source_ids = possible_source_ids[:size]
+                    possible_source_ids = possible_source_ids[size:]
+                    self.add_edge(
+                        AncillaConnection(
+                            (source.raw, source_ids),
+                            (node.raw, target_ids),
+                        ),
+                    )
+                    if len(possible_source_ids) == 0:
+                        self.reusable.remove(source)
+
+                if (
+                    len(need_reusable) > 0 and len(self.uncomputable) > 0
+                ):  # means that len(self.reusable) is empty
+                    new_reusable = self.uncomputable.pop()  # TODO: better heuristik?
+                    self.reusable.append(new_reusable)
+                    new_reusable.info.io.qubits.returned_reusable_ids = new_reusable.info.io.qubits.returned_reusable_after_uncompute_ids
+                    self.uncompute_node(new_reusable)
+
+            while len(need_reusable) > 0 and len(self.uncomputable) > 0:
+                source = self.uncomputable[0]
+                possible_source_ids = (
+                    source.info.io.qubits.returned_reusable_after_uncompute_ids
+                )
                 size = min(len(need_reusable), len(possible_source_ids))
                 target_ids = need_reusable[:size]
                 need_reusable = need_reusable[size:]
@@ -171,42 +226,62 @@ class NoPredDummy(AlgoPerf):
 
             returned_dirty = node.info.io.qubits.returned_dirty_ids
             returned_reusable = node.info.io.qubits.returned_reusable_ids
-            returned_uncompute = (
+            returned_uncomputable = (
                 node.info.io.qubits.returned_reusable_after_uncompute_ids
             )
             if len(returned_dirty) > 0:
                 self.dirty.append(node)
             if len(returned_reusable) > 0:
                 self.reusable.append(node)
+            if len(returned_uncomputable) > 0:
+                self.uncomputable.append(node)
 
         return super().compute()
 
 
-class NoPredSimple(NoPredDummy):
+class NoPredReturnedReusable(NoPredDummy):
     @override
     def get_and_remove_next_nopred(self) -> ProcessedProgramNode:
         self.nopred.sort(
-            key=lambda x: len(x.info.io.qubits.returned_reusable_ids),
+            key=lambda x: len(x.info.io.qubits.returned_reusable_ids)
+            + len(x.info.io.qubits.returned_reusable_after_uncompute_ids),
             reverse=True,
         )
         result, self.nopred = self.nopred[0], self.nopred[1:]
         return result
 
 
+class NoPredRequiredReusable(NoPredDummy):
+    @override
+    def get_and_remove_next_nopred(self) -> ProcessedProgramNode:
+        self.nopred.sort(
+            key=lambda x: len(x.info.io.qubits.required_reusable_ids),
+            reverse=False,
+        )
+        result, self.nopred = self.nopred[0], self.nopred[1:]
+        return result
+
+
 def main() -> None:
-    contenders: dict[type[AlgoPerf], int] = {
-        DummyAlgo: 0,
-        NoPredDummy: 0,
-        NoPredSimple: 0,
+    contenders: dict[type[AlgoPerf], tuple[int, int]] = {
+        DummyAlgo: (0, 0),
+        NoPredDummy: (0, 0),
+        NoPredReturnedReusable: (0, 0),
+        NoPredRequiredReusable: (0, 0),
     }
     for _ in range(100):
         graph = random_graph(50)
-        for algo in contenders:
+        for algo, cur in contenders.items():
             instance = algo(deepcopy(graph))
-            contenders[algo] += instance.compute()
+            perf, uncomputed = instance.compute()
+            contenders[algo] = cur[0] + perf, cur[1] + uncomputed
 
-    for algo, perf in sorted(contenders.items(), key=lambda x: x[1], reverse=True):
-        print(f"{algo.__name__} -> {perf}")
+    for algo, perf_uncomp in sorted(
+        contenders.items(),
+        key=lambda x: x[1][1],
+        reverse=True,
+    ):
+        print(f"{algo.__name__} -> {perf_uncomp}")
 
 
 if __name__ == "__main__":
