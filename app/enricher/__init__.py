7e02491fa3608e1e771a8d3d7b85404c53dbb7b1
Lukas Kurz <lukas@shortdev.de>
2025-04-23 21:25:49 +0200

    Enricher api

---
+++ app/enricher/__init__.py
@@ -0,0 +1,93 @@
+"""
+The enrichment module provides the abstract capability of "enriching" :class:`~app.model.CompileRequest.Node` with an implementation
+(See :class:`~app.model.CompileRequest.ImplementationNode`).
+
+The enrichment can be controlled by specifying :class:`~app.enricher.Constraints`.
+
+Multiple enrichment-services can be connected to the backend by supplying implementations of :class:`~app.enricher.Enricher`.
+Some services could read implementations from a database or generate them on the fly.
+"""
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from io import UnsupportedOperation
+
+from app.model.CompileRequest import (
+    ImplementationNode,
+    Node,
+)
+from app.processing.graph import ClassicalIOInstance, QubitIOInstance
+
+
+@dataclass
+class Constraints:
+    """
+    Constraints to follow during enrichment.
+    """
+
+    requested_inputs: list[QubitIOInstance | ClassicalIOInstance]
+
+
+def empty_constraints() -> Constraints:
+    """
+    No constraints.
+    """
+
+    return Constraints([])
+
+
+class Enricher(ABC):
+    """
+    An enrichment-unit capable of enriching some nodes.
+    """
+
+    @abstractmethod
+    def try_enrich(
+        self, node: Node, constraints: Constraints
+    ) -> ImplementationNode | None:
+        """
+        Tries to enrich the given node according to the specified constraints.
+        Returns none if enrichment is not possible.
+
+        :param node: The node to enrich.
+        :param constraints: Constraints to follow during enrichment.
+        :return: The enriched node or none if no enriched can be found.
+        """
+        raise NotImplementedError()
+
+    def enrich(self, node: Node, constraints: Constraints) -> ImplementationNode:
+        """
+        Enrich the given node according to the specified constraints.
+        Throws if enrichment is not possible.
+
+        :param node: The node to enrich.
+        :param constraints: Constraints to follow during enrichment.
+        :return: The enriched node.
+        """
+
+        enriched = self.try_enrich(node, constraints)
+        if enriched is not None:
+            return enriched
+
+        raise UnsupportedOperation(f"Unsupported node '{type(node).__name__}'")
+
+
+class AggregateEnricher(Enricher):
+    """
+    An :class:`~app.enricher.Enricher` that combines multiple enrichers into one.
+    """
+
+    enrichers: list[Enricher]
+
+    def __init__(self, *enrichers: Enricher):
+        self.enrichers = list(enrichers)
+
+    def try_enrich(
+        self, node: Node, constraints: Constraints
+    ) -> ImplementationNode | None:
+        for enricher in self.enrichers:
+            enriched = enricher.try_enrich(node, constraints)
+            if enriched is not None:
+                return enriched
+
+        return None
---
+++ tests/enricher/test_init.py
@@ -0,0 +1,124 @@
+from io import UnsupportedOperation
+
+import pytest
+
+from app.enricher import AggregateEnricher, Constraints, Enricher, empty_constraints
+from app.model.CompileRequest import (
+    BoolLiteralNode,
+    FloatLiteralNode,
+    ImplementationNode,
+    IntLiteralNode,
+    Node,
+)
+
+
+class IntToAEnricher(Enricher):
+    def try_enrich(
+        self, node: Node, _constraints: Constraints
+    ) -> ImplementationNode | None:
+        if not isinstance(node, IntLiteralNode):
+            return None
+
+        return ImplementationNode(id=node.id, implementation="A")
+
+
+class FloatToBEnricher(Enricher):
+    def try_enrich(
+        self, node: Node, _constraints: Constraints
+    ) -> ImplementationNode | None:
+        if not isinstance(node, FloatLiteralNode):
+            return None
+
+        return ImplementationNode(id=node.id, implementation="B")
+
+
+def test_try_enrich_known_node() -> None:
+    enricher = IntToAEnricher()
+    enriched_node = enricher.try_enrich(
+        IntLiteralNode(id="nodeId", value=42), empty_constraints()
+    )
+
+    assert enriched_node is not None
+    assert enriched_node.id == "nodeId"
+    assert enriched_node.implementation == "A"
+
+
+def test_enrich_known_node() -> None:
+    enricher = IntToAEnricher()
+    enriched_node = enricher.enrich(
+        IntLiteralNode(id="nodeId", value=42), empty_constraints()
+    )
+
+    assert enriched_node is not None
+    assert enriched_node.id == "nodeId"
+    assert enriched_node.implementation == "A"
+
+
+def test_try_enrich_unknown_node() -> None:
+    enricher = IntToAEnricher()
+    enriched_node = enricher.try_enrich(
+        FloatLiteralNode(id="nodeId", value=42.0), empty_constraints()
+    )
+
+    assert enriched_node is None
+
+
+def test_enrich_unknown_node() -> None:
+    enricher = IntToAEnricher()
+    with pytest.raises(
+        UnsupportedOperation, match="^Unsupported node 'FloatLiteralNode'$"
+    ):
+        enricher.enrich(FloatLiteralNode(id="nodeId", value=42.0), empty_constraints())
+
+
+def test_aggregate_enrich_known_node() -> None:
+    enricher = AggregateEnricher(IntToAEnricher(), FloatToBEnricher())
+    enriched_node = enricher.enrich(
+        IntLiteralNode(id="nodeId", value=42), empty_constraints()
+    )
+
+    assert enriched_node.id == "nodeId"
+    assert enriched_node.implementation == "A"
+
+    enriched_node = enricher.enrich(
+        FloatLiteralNode(id="nodeId2", value=42.0), empty_constraints()
+    )
+
+    assert enriched_node.id == "nodeId2"
+    assert enriched_node.implementation == "B"
+
+
+def test_aggregate_try_enrich_known_node() -> None:
+    enricher = AggregateEnricher(IntToAEnricher(), FloatToBEnricher())
+    enriched_node = enricher.try_enrich(
+        IntLiteralNode(id="nodeId", value=42), empty_constraints()
+    )
+
+    assert enriched_node is not None
+    assert enriched_node.id == "nodeId"
+    assert enriched_node.implementation == "A"
+
+    enriched_node = enricher.try_enrich(
+        FloatLiteralNode(id="nodeId2", value=42.0), empty_constraints()
+    )
+
+    assert enriched_node is not None
+    assert enriched_node.id == "nodeId2"
+    assert enriched_node.implementation == "B"
+
+
+def test_aggregate_try_enrich_unknown_node() -> None:
+    enricher = AggregateEnricher(IntToAEnricher(), FloatToBEnricher())
+    enriched_node = enricher.try_enrich(
+        BoolLiteralNode(id="nodeId", value=False), empty_constraints()
+    )
+
+    assert enriched_node is None
+
+
+def test_aggregate_enrich_unknown_node() -> None:
+    enricher = AggregateEnricher(IntToAEnricher(), FloatToBEnricher())
+    with pytest.raises(
+        UnsupportedOperation, match="^Unsupported node 'BoolLiteralNode'$"
+    ):
+        enricher.enrich(BoolLiteralNode(id="nodeId", value=False), empty_constraints())
