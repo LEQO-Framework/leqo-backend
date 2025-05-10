c9f0a0ab2465083130604e72e2ced9fedb5e1f96
Louis Radek <radek.louis@gmail.com>
2025-05-10 14:08:31 +0200

    feat: add prepare state node

---
+++ app/enricher/prepare_state.py
@@ -0,0 +1,77 @@
+"""
+Provides enricher strategy for enriching :class:`~app.model.CompileRequest.PrepareStateNode` from a database.
+"""
+
+from typing import override
+
+from sqlalchemy import and_, select
+
+from app.enricher import (
+    Constraints,
+    ConstraintValidationException,
+    EnricherStrategy,
+    EnrichmentResult,
+    ImplementationMetaData,
+    NodeUnsupportedException,
+)
+from app.enricher.engine import DatabaseEngine
+from app.enricher.models import BaseNode as BaseNodeTable
+from app.enricher.models import PrepareStateNode as PrepareStateTable
+from app.enricher.utils import implementation
+from app.model.CompileRequest import (
+    Node as FrontendNode,
+)
+from app.model.CompileRequest import (
+    PrepareStateNode,
+)
+
+
+# ToDo: bound and size are unclear
+class PrepareStateEnricherStrategy(EnricherStrategy):
+    """
+    Strategy capable of enriching :class:`~app.model.CompileRequest.PrepareStateNode` from a database.
+    """
+
+    @override
+    def _enrich_impl(
+        self, node: FrontendNode, constraints: Constraints | None
+    ) -> EnrichmentResult:
+        if not isinstance(node, PrepareStateNode):
+            raise NodeUnsupportedException(node)
+
+        if (
+            constraints is None or len(constraints.requested_inputs) != 0
+        ):  # How do Ancilla nodes count?????
+            raise ConstraintValidationException("PrepareStateNode can't have an input")
+
+        databaseEngine = DatabaseEngine()
+        session = databaseEngine._get_database_session()
+        query = (
+            select(PrepareStateTable)
+            .join(BaseNodeTable, PrepareStateTable.id == BaseNodeTable.id)
+            .where(
+                and_(
+                    PrepareStateTable.type == node.type,
+                    PrepareStateTable.depth <= constraints.optimizeDepth,
+                    PrepareStateTable.width <= constraints.optimizeWidth,
+                    PrepareStateTable.inputs == [],
+                    PrepareStateTable.size == node.size,
+                    PrepareStateTable.quantum_state == node.quantumState,
+                )
+            )
+        )
+
+        result_data = session.execute(query).scalars().first()
+
+        if result_data is None:
+            raise NodeUnsupportedException(node)
+
+        return EnrichmentResult(
+            implementation(
+                node,
+                [
+                    # convert implementation into program ????
+                ],
+            ),
+            ImplementationMetaData(width=result_data.width, depth=result_data.depth),
+        )
