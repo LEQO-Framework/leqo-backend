5c768351cafa97843ed6aa3490b19ae419863c45
michael.kica <kica819@gmail.com>
2026-04-23 17:27:26 +0300

    repeat block fix

--- app/transformation_manager/nested/repeat.py
+++ app/transformation_manager/nested/repeat.py
@@ -33,47 +33,52 @@ def unroll_repeat(
         msg = f"RepeatNode can't have {node.iterations} < 1 iterations."
         raise RepeatException(msg, node)
 
-    parent_id = ProgramNode(node.id).id
-    pass_node_impl = generate_pass_node_implementation(requested_inputs)
+    try:
+        parent_id = ProgramNode(node.id).id
+        pass_node_impl = generate_pass_node_implementation(requested_inputs)
 
-    result = FrontendGraph()
-    entry_node = ParsedImplementationNode(
-        id=f"leqo_{parent_id.hex}_repeat_entry", implementation=deepcopy(pass_node_impl)
-    )
-    result.append_node(entry_node)
-    prev_id = entry_node.id
-    exit_node: ParsedImplementationNode
-    for i in range(node.iterations):
-
-        def new_id(old_id: str, i: int = i) -> str:
-            return f"leqo_{parent_id.hex}_repeat_{i}_{old_id}"
-
-        exit_node = ParsedImplementationNode(
-            id=f"leqo_{parent_id.hex}_repeat_{i}_exit",
-            implementation=deepcopy(pass_node_impl),
+        result = FrontendGraph()
+        entry_node = ParsedImplementationNode(
+            id=f"leqo_{parent_id.hex}_repeat_entry", implementation=deepcopy(pass_node_impl)
         )
-        result.append_node(exit_node)
-        for n in node.block.nodes:
-            if n.id == node.id:
-                continue
-            iter_node = deepcopy(n)
-            iter_node.id = new_id(iter_node.id)
-            result.append_node(iter_node)
-        for e in node.block.edges:
-            iter_edge = deepcopy(e)
-            source_id = iter_edge.source[0]
-            target_id = iter_edge.target[0]
-            if source_id == node.id:
-                iter_edge.source = (prev_id, iter_edge.source[1])
-            else:
-                iter_edge.source = (new_id(source_id), iter_edge.source[1])
-            if target_id == node.id:
-                iter_edge.target = (exit_node.id, iter_edge.target[1])
-            else:
-                iter_edge.target = (new_id(target_id), iter_edge.target[1])
-            result.append_edge(iter_edge)
+        result.append_node(entry_node)
+        prev_id = entry_node.id
+        exit_node: ParsedImplementationNode
+        for i in range(node.iterations):
 
-        prev_id = exit_node.id
+            def new_id(old_id: str, i: int = i) -> str:
+                return f"leqo_{parent_id.hex}_repeat_{i}_{old_id}"
 
-    result.rename_nodes({exit_node.id: node.id})
-    return (entry_node.id, exit_node.id, result)
+            exit_node = ParsedImplementationNode(
+                id=f"leqo_{parent_id.hex}_repeat_{i}_exit",
+                implementation=deepcopy(pass_node_impl),
+            )
+            result.append_node(exit_node)
+            for n in node.block.nodes:
+                if n.id == node.id:
+                    continue
+                iter_node = deepcopy(n)
+                iter_node.id = new_id(iter_node.id)
+                result.append_node(iter_node)
+            for e in node.block.edges:
+                iter_edge = deepcopy(e)
+                source_id = iter_edge.source[0]
+                target_id = iter_edge.target[0]
+                if source_id == node.id:
+                    iter_edge.source = (prev_id, iter_edge.source[1])
+                else:
+                    iter_edge.source = (new_id(source_id), iter_edge.source[1])
+                if target_id == node.id:
+                    iter_edge.target = (exit_node.id, iter_edge.target[1])
+                else:
+                    iter_edge.target = (new_id(target_id), iter_edge.target[1])
+                result.append_edge(iter_edge)
+
+            prev_id = exit_node.id
+
+        result.rename_nodes({exit_node.id: node.id})
+        return (entry_node.id, exit_node.id, result)
+        
+    except Exception as exc:
+        msg = f"Failed to unroll Repeat block. Please check the internal wiring of the nodes inside the loop. Details: {exc}"
+        raise RepeatException(msg, node) from exc
